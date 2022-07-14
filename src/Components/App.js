f3402869c68b15d5e3879899459da2dbb9a3be50
Tushar Chaudhari <45455985+Tushar-Chaudhari-04@users.noreply.github.com>
2022-07-14 12:48:43 +0530

    Google Keeper Modified to 2.0

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,35 +1,40 @@
 import React from "react";
 import Card from "./Card";
 import Contacts from "./Contacts";
-
-function CardDetails(data){
-    return <Card
-        id={data.id}
-        name={data.name}
-        src={data.imgURL}
-        num={data.phone}
-        email={data.email}
-      />;
-}
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+import Data from "../data";
 
 function App() {
-    return <div>
-      <h1 className="heading">My Contacts</h1>
-      {Contacts.map(CardDetails)}
+  return (
+    <div>
+      <Header />
+      {Data.map((item) => (
+        <Note title={item.title} content={item.content} />
+      ))}
+      <Footer />
     </div>
+  );
 }
 
 export default App;
 
-
-
 /*
-function App(){
-    return <div>
-        <Header />
-        <Note />
-        <Footer />
-    </div>;
+    function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      {Contacts.map((data) => (
+        <Card
+          id={data.id}
+          name={data.name}
+          src={data.imgURL}
+          num={data.phone}
+          email={data.email}
+        />
+      ))}
+    </div>
+  );
 }
-
 */
---
+++ src/data.js
@@ -0,0 +1,9 @@
+const Data = [
+  {
+    id: 1,
+    title: "Web Dev",
+    content: "MERN Stack stands for MongoDB,Express Js,React Js,Node"
+  }
+];
+
+export default Data;
